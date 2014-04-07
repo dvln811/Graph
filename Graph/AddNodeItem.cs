5177920e7e8ce18ca6acec7bc6220e85bf47b68c
dvln811 <devlyn@dlnapoli.com>
2014-04-07 16:08:36 -0400

    Adding AddNodeItem to add node items from the Node itself

---
+++ Graph/AddNodeItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+
+namespace Graph
+{
+    internal sealed class AddNodeItem : NodeItem
+    {
+        internal override SizeF Measure(Graphics context)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal override void Render(Graphics graphics, SizeF minimumSize, PointF position)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
