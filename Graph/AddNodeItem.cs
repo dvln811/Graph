db482fdf8c9427a20860d018064322d8864bc863
dvln811 <devlyn@dlnapoli.com>
2014-04-07 16:15:46 -0400

    Building AddNodeItem.

--- Graph/AddNodeItem.cs
+++ Graph/AddNodeItem.cs
@@ -8,6 +8,9 @@ namespace Graph
 {
     internal sealed class AddNodeItem : NodeItem
     {
+        private string internalTitle;
+        public string InternalTitle { get { return internalTitle; } set { internalTitle = value; } }
+        
         internal override SizeF Measure(Graphics context)
         {
             throw new NotImplementedException();
@@ -15,7 +18,19 @@ namespace Graph
 
         internal override void Render(Graphics graphics, SizeF minimumSize, PointF position)
         {
-            throw new NotImplementedException();
+            var size = Measure(graphics);
+            size.Width = Math.Max(minimumSize.Width, size.Width);
+            size.Height = Math.Max(minimumSize.Height, size.Height);
+
+            size.Height -= GraphConstants.TopHeight;
+            position.Y += GraphConstants.TopHeight;
+
+            if ((state & RenderState.Hover) == RenderState.Hover)
+                graphics.DrawString(this.InternalTitle, SystemFonts.CaptionFont, Brushes.White, new RectangleF(position, size), 
+                    GraphConstants.TitleStringFormat);
+            else
+                graphics.DrawString(this.InternalTitle, SystemFonts.CaptionFont, Brushes.Black, new RectangleF(position, size), 
+                    GraphConstants.TitleStringFormat);
         }
     }
 }
