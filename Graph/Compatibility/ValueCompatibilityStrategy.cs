c7fbcbcd79344ff4774ad3ad60bd826168e040e5
dvln811 <devlyn@dlnapoli.com>
2014-04-09 09:20:26 -0400

    Added ValueCompatibiltyStrategy

---
+++ Graph/Compatibility/ValueCompatibilityStrategy.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Graph.Compatibility
+{
+    class ValueCompatibilityStrategy : ICompatibilityStrategy
+    {
+        public bool CanConnect(NodeConnector from, NodeConnector to)
+        {
+            if (null == from.Item.Tag || null == to.Item.Tag) return false;
+
+            var fromStr = from.Item.Tag as string;
+            var toStr = to.Item.Tag as string;
+
+            if (fromStr.Equals(toStr))
+                return true;
+
+            return false;
+        }
+    }
+}
