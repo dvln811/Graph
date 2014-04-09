72d198575119d26b782d7876c7604076b19c6899
dvln811 <devlyn@dlnapoli.com>
2014-04-09 11:25:21 -0400

    Building

---
+++ Graph/Items/NodeMultiItem.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+
+namespace Graph.Items
+{
+    public class NodeMultiItem : NodeItem
+    {
+        List<NodeItem> Items { get; set; }
+        AddNodeItem AddNodeItem { get; set; }
+
+        public NodeMultiItem(List<NodeItem> items)
+        {
+            Items = items;
+        }
+
+        internal override SizeF Measure(Graphics context)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal override void Render(Graphics graphics, SizeF minimumSize, PointF position)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
